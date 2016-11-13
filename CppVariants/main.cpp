473baca127e8044606606a4daf3ec075f7026921
Uli Kusterer <ulivc@zathras.de>
2016-11-13 14:02:41 +0100

    Initial Commit

---
+++ CppVariants/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppVariants
+//
+//  Created by Uli Kusterer on 13/11/2016.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
